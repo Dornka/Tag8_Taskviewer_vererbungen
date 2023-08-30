0537b812d5d161ed04b308c8ac318492a205fea5
Dornka <Skiber@gmail.com>
2023-08-30 11:20:46 +0200

    added classes, methods and main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,48 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+class Fahrzeug {
+    private String hersteller;
+    private String modell;
+    private int baujahr;
+
+    public Fahrzeug(String hersteller, String modell, int baujahr) {
+        this.hersteller = hersteller;
+        this.modell = modell;
+        this.baujahr = baujahr;
+    }
+
+    public void printInfo() {
+        System.out.println("Hersteller: " + hersteller);
+        System.out.println("Modell: " + modell);
+        System.out.println("Baujahr: " + baujahr);
+    }
+}
+
+class Auto extends Fahrzeug {
+    private int anzahlTueren;
+
+    public Auto(String hersteller, String modell, int baujahr, int anzahlTueren) {
+        super(hersteller, modell, baujahr);
+        this.anzahlTueren = anzahlTueren;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Anzahl der Türen: " + anzahlTueren);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Fahrzeug fahrzeug = new Fahrzeug("Hersteller1", "Modell1", 2020);
+        Auto auto = new Auto("Hersteller2", "Modell2", 2022, 4);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Informationen über das Fahrzeug:");
+        fahrzeug.printInfo();
+
+        System.out.println("\nInformationen über das Auto:");
+        auto.printInfo();
     }
 }
