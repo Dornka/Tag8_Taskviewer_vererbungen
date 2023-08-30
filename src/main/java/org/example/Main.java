31217403492711aa4d9cbbb7668a4d071e1abe4b
Dornka <Skiber@gmail.com>
2023-08-30 11:36:48 +0200

    added class Motorrad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -32,17 +32,34 @@ class Auto extends Fahrzeug {
         System.out.println("Anzahl der Türen: " + anzahlTueren);
     }
 }
+class Motorrad extends Fahrzeug {
+    private String motorradArt;
+
+    public Motorrad(String hersteller, String modell, int baujahr, String motorradArt) {
+        super(hersteller, modell, baujahr);
+        this.motorradArt = motorradArt;
+    }
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Art des Motorrads: " + motorradArt);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
 
         Fahrzeug fahrzeug = new Fahrzeug("Hersteller1", "Modell1", 2020);
         Auto auto = new Auto("Hersteller2", "Modell2", 2022, 4);
+        Motorrad motorrad = new Motorrad("Hersteller3", "Modell3", 2021, "Sportbike");
 
         System.out.println("Informationen über das Fahrzeug:");
         fahrzeug.printInfo();
 
         System.out.println("\nInformationen über das Auto:");
         auto.printInfo();
+
+        System.out.println("\nInformationen über das Motorrad:");
+        motorrad.printInfo();
     }
 }
